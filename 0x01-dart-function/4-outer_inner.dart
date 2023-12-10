023c46dd28c7df285e55c1f5c4987637a215dab6
NehemieMbg <nehemie.mbg@gmail.com>
2023-12-10 19:48:29 +0100

    outer

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,16 +1,9 @@
-String inner() {
-  return 'Hello Agent B.';
-}
-
 void outer(String name, String id) {
-  List<String> nameParts = name.split(' ');
-  String firstName = nameParts[0];
-  String lastName = nameParts[1].substring(0, 1);
-  String greeting = inner();
+  String inner() {
+    var splitter = name.split(" ");
+    var alias = splitter[1].substring(0, 1) + "." + splitter[0];
+    return "Hello Agent ${alias} your id is $id";
+  }
 
-  print('$greeting$lastName.$firstName your id is $id');
-}
-
-void main() {
-  outer("Youssef Belhadj", "001");
+  print(inner());
 }
