20bc05066abd7fc3c042ad631ac3d35f10930887
NehemieMbg <nehemie.mbg@gmail.com>
2023-12-10 19:45:25 +0100

    dart

---
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -0,0 +1,39 @@
+bool isPalindrome(String s) {
+  int start = 0;
+  int end = s.length - 1;
+
+  while (start < end) {
+    if (s[start] != s[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
+}
+
+String longestPalindrome(String s) {
+  if (s.length < 3) {
+    return "none";
+  }
+
+  String longest = "";
+
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i + 2; j <= s.length; j++) {
+      String substring = s.substring(i, j);
+      if (isPalindrome(substring) && substring.length > longest.length) {
+        longest = substring;
+      }
+    }
+  }
+
+  return longest.isNotEmpty ? longest : "none";
+}
+
+void main() {
+  print(longestPalindrome("abcde"));
+  print(longestPalindrome("azeghjhg"));
+  print(longestPalindrome("aaabvf"));
+}
---
+++ 0x01-dart-function/9-palindrome.dart
@@ -0,0 +1,24 @@
+bool isPalindrome(String s) {
+  if (s.length < 3) {
+    return false;
+  }
+
+  int start = 0;
+  int end = s.length - 1;
+
+  while (start < end) {
+    if (s[start] != s[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
+}
+
+void main() {
+  print(isPalindrome("aa"));
+  print(isPalindrome("abcba"));
+  print(isPalindrome("abcde"));
+}
